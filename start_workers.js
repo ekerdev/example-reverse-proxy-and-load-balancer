dfc309c5fa3e0a6f7baf7d1e62ceeb158a82c7ea
Erik Marques <erik.marques@ekerdev.com>
2025-01-24 18:57:57 -0300

    fix path of server.js

--- start_workers.js
+++ start_workers.js
@@ -2,7 +2,7 @@ import { Worker } from "node:worker_threads";
 import path from "node:path";
 
 for (let i = 0; i < 5; i++) {
-    const worker = new Worker(path.resolve(process.cwd(), "api/server.js"), {
+    const worker = new Worker(path.resolve(process.cwd(), "server.js"), {
         workerData: {
             port: 8081 + i,
             instanceId: i
